6e332a53e46e4ce297cdb4c42f6755c5c7d26376
coronviv <vivian.j.coronado@gmail.com>
2018-05-23 14:33:42 -0400

    If Else practice

--- mix-list3/animal-list.py
+++ mix-list3/animal-list.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
 list1 = ['Fox', 'Fly', 'Ant', 'Bee', 'Cod', 'Cat', 'Dog']
-list1.extend(['Yak', 'Cow', 'Hen', 'Koi', 'Hog', 'Jay', 'Kit'])
+list1.extend(['Yak', 'Cow', 'Hen', 'Koi'])
+list1.append(['Hog', 'Jay', 'Kit'])
 print(list1)
