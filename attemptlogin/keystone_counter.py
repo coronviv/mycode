fbb1ddace63fa562c4ba756692b3b5836c8b1b96
coronviv <vivian.j.coronado@gmail.com>
2018-05-24 16:34:16 -0400

    Parsing log files in progress

---
+++ attemptlogin/keystone_counter.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# parse keystone.common.wsgi and return number of failed login attempts
+loginfail = 0
+keystone_file = open('home/student/mycode/attemptlogin/keystone.common.wsgi', 'r')
+keystone_file_lines=keystone_file.readlines()
+for i in range(len(keystone_file_lines)):
+    if "- - - - -] Authorization failed" in keystone_file_lines[i]:
+        loginfail += 1 # this is the same as loginfail = loginfail + 1
+print('The number of failed log in attempts is ' + str(loginfail))
