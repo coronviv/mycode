b9643bb050ef40220f78d7bfa43bda87a5d968ce
coronviv <vivian.j.coronado@gmail.com>
2018-05-30 18:51:18 -0400

    Input practice

---
+++ lab_input/input_ip.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+user_input = input('Please enter an IPv4 IP address:')
+print("You told me the IPv4 address is:" + user_input)
+vendor_input = input('Please provide the vendor name associated with the device:')
+print("You told me the Vendor Name is:" + vendor_input)
+
